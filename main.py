48c35c9ccde7da5a3d6130908904ef03aae99e12
Md. Mainul Islam (Faruqi) <programmermainul@gmail.com>
2021-01-13 16:14:46 +0000

    enumerate

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ is_User = True
 
 if is_Friend and is_User:
   print("best friend forever")
+
+# enumerate
+
+for i, char in enumerate(list(range(100))):
+  print(i,char)
+  if char == 50:
+    print(f"the index of 50 {i}")
