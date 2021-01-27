86cf9b25967ae96b535e61f6c4e2e39d2d81ea43
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-01-27 23:13:52 +0600

    more error

--- Python Handling Error/errors.py
+++ Python Handling Error/errors.py
@@ -6,8 +6,13 @@ while True:
         10/age
     except ValueError:
         print('Please enter a number')
+        continue # Trying again from the top
     except ZeroDivisionError:
         print('Please enter age higher than 0')
+        break # Getting out from the loop but finally block will run
     else:
         print('Thank you')
-        break
+        break # Getting out from the loop but finally block will run
+    finally:
+        print('ok, I am finally done')
+    print('can you hear me?')
